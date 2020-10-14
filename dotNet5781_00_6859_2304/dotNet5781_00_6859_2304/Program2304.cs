0afc9cba8bff231e3b5bb13b71e2c3778717dfbf
alexandre lejdstrom <yehouda.lejdstrom@gmail.com>
2020-10-14 21:37:49 +0300

    New class + Welcome2304

---
+++ dotNet5781_00_6859_2304/dotNet5781_00_6859_2304/Program2304.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_6859_2304
+{
+    partial class Program 
+    {
+        static partial void Welcome2304()
+        {
+            Console.WriteLine("I am also here !");
+        }
+    }
+}
