30cf3f5e50d79f853d5646d7bc1a49d357b8a195
samuel verse <verse@g.jct.ac.il>
2020-10-14 21:19:15 +0300

    initial code * Welcome2304

---
+++ dotNet5781_00_6859_2304/dotNet5781_00_6859_2304/Program9875.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_6859_2304
+{
+
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome6859();
+            Welcome2304();
+
+            Console.ReadKey();
+        }
+        static partial void Welcome2304();
+        private static void Welcome6859()
+        {
+            Console.WriteLine("Enter your name: ");
+            String str = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", str);
+        }
+    }
+
+}
