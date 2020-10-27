caff023235b456cf0661cc32f7e90e0687738f8a
samuel verse <verse@g.jct.ac.il>
2020-10-27 23:11:24 +0200

    functions and linkedlist

--- dotNet5781_00_6859_2304/doNet5781_01_6859_2304/Bus.cs
+++ dotNet5781_00_6859_2304/doNet5781_01_6859_2304/Bus.cs
@@ -7,18 +7,39 @@ namespace doNet5781_01_6859_2304
     class Bus
     {
         private string busId;
-        private DateTime dateInit ;
+        private DateTime dateInit;
         private int km;
         private int tank;
 
-        public void SetBusId (string newId) { busId = newId;}
-        public void SetDate (DateTime newDate) {dateInit = newDate;}
-        public void SetKm (int newKm) { km = newKm;}
-        public void SetTank (int newTank) { tank = newTank;}
+        public void SetBusId(string newId) { busId = newId; }
+        public void SetDate(DateTime newDate) { dateInit = newDate; }
+        public void SetKm(int newKm) { km = newKm; }
+        public void SetTank(int newTank) { tank = newTank; }
 
         public string GetId() { return busId; }
         public DateTime GetDate() { return dateInit; }
         public int GetKm() { return km; }
-        public int GetTank() {return tank;}
+        public int GetTank() { return tank; }
+
+        public static void addBus(DateTime Date, string Id)
+        {
+            Bus bus = new Bus();
+            bus.SetDate(Date);
+            bus.SetBusId(Id);
+
+        }
+        public static Bus ChooseBus(string Id, LinkedList<Bus> busList)
+        {
+            //new Random(DateTime.Now.Millisecond)
+            //foreach (string id in busList)
+            //{
+
+            //    return Bus;
+            //}
+
+
+            Console.WriteLine("ERROR id doesn't exist");
+            return null;
+        }
     }
 }
--- dotNet5781_00_6859_2304/doNet5781_01_6859_2304/Program.cs
+++ dotNet5781_00_6859_2304/doNet5781_01_6859_2304/Program.cs
@@ -1,19 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace doNet5781_01_6859_2304
 {
     class Program
     {
         enum Choices
-	{
+        {
             ADD, CHOOSE_BUS, TREAT, PRINT_TREATS, EXIT // treat = treatement
-	};
-	
+        };
+
         static void Main(string[] args)
         {
-  
+            LinkedList<Bus> busList = new LinkedList<Bus>();
 
-	
+
+
         }
     }
 }
