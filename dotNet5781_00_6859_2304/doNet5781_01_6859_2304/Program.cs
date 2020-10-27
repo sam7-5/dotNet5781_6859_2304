2a06a4e5b6ecb7da7952d7bb89c2dfef8d1e0e98
alexandre lejdstrom <yehouda.lejdstrom@gmail.com>
2020-10-27 23:12:33 +0200

    added console menu

--- dotNet5781_00_6859_2304/doNet5781_01_6859_2304/Program.cs
+++ dotNet5781_00_6859_2304/doNet5781_01_6859_2304/Program.cs
@@ -1,19 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace doNet5781_01_6859_2304
 {
     class Program
     {
-        enum Choices
-	{
-            ADD, CHOOSE_BUS, TREAT, PRINT_TREATS, EXIT // treat = treatement
-	};
 	
         static void Main(string[] args)
         {
-  
-
+            bool showMenu = true;
+            while (showMenu){
+                showMenu = MainMenu();
+            }   
 	
         }
+
+        public static LinkedList<Bus> busList = new LinkedList<Bus>();
+
+        private static bool MainMenu()
+            {
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) add a bus to the bus list");
+            Console.WriteLine("2) choose a bus to travel");
+            Console.WriteLine("3) treatement of a bus");
+            Console.WriteLine("4) print all the buses");
+            Console.WriteLine("5) exit");
+
+            switch(Console.Read())
+            {
+                case "1":
+                //add
+                return true;
+
+                case "2":
+                //choose
+                return true;
+               
+                case "3":
+                //treatement
+                return true;
+                
+                case "4":
+                // print
+                return true;
+
+                case "5":
+                return false;
+
+                default:
+                return true;
+            }
     }
-}
+} 
