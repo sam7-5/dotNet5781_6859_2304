2fbf60c14834eab26d5d4d4c6aae97d296d344f3
samuel verse <verse@g.jct.ac.il>
2020-10-27 21:55:38 +0200

    class

---
+++ dotNet5781_00_6859_2304/doNet5781_01_6859_2304/Bus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace doNet5781_01_6859_2304
+{
+    class Bus
+    {
+        string busId;
+        DateTime dateInit ;
+        int km;
+        int tank;
+
+        
+            
+     
+    }
+}
---
+++ dotNet5781_00_6859_2304/doNet5781_01_6859_2304/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace doNet5781_01_6859_2304
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
