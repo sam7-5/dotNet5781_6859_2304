85bc13d19f7542f36650bace1b0a5d7ecef625ac
alexandre lejdstrom <yehouda.lejdstrom@gmail.com>
2020-10-27 22:22:57 +0200

    Added enum to program.cs

--- dotNet5781_00_6859_2304/doNet5781_01_6859_2304/Program.cs
+++ dotNet5781_00_6859_2304/doNet5781_01_6859_2304/Program.cs
@@ -4,9 +4,16 @@ namespace doNet5781_01_6859_2304
 {
     class Program
     {
+        enum Choices
+	{
+            ADD, CHOOSE_BUS, TREAT, PRINT_TREATS, EXIT // treat = treatement
+	};
+	
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+  
+
+	
         }
     }
 }
