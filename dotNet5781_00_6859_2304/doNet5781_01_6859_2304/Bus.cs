05744a6bfe13b53a1a305ace6d4faa631de9fa22
alexandre lejdstrom <yehouda.lejdstrom@gmail.com>
2020-10-27 22:12:25 +0200

    class bus setters and getters

--- dotNet5781_00_6859_2304/doNet5781_01_6859_2304/Bus.cs
+++ dotNet5781_00_6859_2304/doNet5781_01_6859_2304/Bus.cs
@@ -6,13 +6,19 @@ namespace doNet5781_01_6859_2304
 {
     class Bus
     {
-        string busId;
-        DateTime dateInit ;
-        int km;
-        int tank;
+        private string busId;
+        private DateTime dateInit ;
+        private int km;
+        private int tank;
 
-        
-            
-     
+        public void SetBusId (string newId) { busId = newId;}
+        public void SetDate (DateTime newDate) {dateInit = newDate;}
+        public void SetKm (int newKm) { km = newKm;}
+        public void SetTank (int newTank) { tank = newTank;}
+
+        public string GetId() { return busId; }
+        public DateTime GetDate() { return dateInit; }
+        public int GetKm() { return km; }
+        public int GetTank() {return tank;}
     }
 }
