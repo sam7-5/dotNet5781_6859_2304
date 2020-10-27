d04775ce923e01247b032825145006f7badc8a36
samuel verse <verse@g.jct.ac.il>
2020-10-27 23:51:49 +0200

    move class

--- dotNet5781_00_6859_2304/doNet5781_01_6859_2304/Program.cs
+++ dotNet5781_00_6859_2304/doNet5781_01_6859_2304/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+  
+
 
 namespace doNet5781_01_6859_2304
 {
+    
     class Program
     {
 	
@@ -19,7 +22,7 @@ namespace doNet5781_01_6859_2304
         public static LinkedList<Bus> busList = new LinkedList<Bus>();
 
         private static bool MainMenu()
-            {
+        {
             Console.Clear();
             Console.WriteLine("Choose an option:");
             Console.WriteLine("1) add a bus to the bus list");
@@ -28,29 +31,68 @@ namespace doNet5781_01_6859_2304
             Console.WriteLine("4) print all the buses");
             Console.WriteLine("5) exit");
 
-            switch(Console.Read())
+            switch (Console.ReadLine())
             {
                 case "1":
-                //add
-                return true;
+                    Bus.addBus(busList);
+                    return true;
 
                 case "2":
-                //choose
-                return true;
-               
+                    //choose
+                    return true;
+
                 case "3":
-                //treatement
-                return true;
-                
+                    //treatement
+                    return true;
+
                 case "4":
-                // print
-                return true;
+                    // print
+                    return true;
 
                 case "5":
-                return false;
+                    return false;
 
                 default:
-                return true;
+                    return true;
             }
+        }
+    }
+    class Bus
+    {
+        private string busId;
+        private DateTime dateInit;
+        private int km;
+        private int tank;
+
+        public void SetBusId(string newId) { busId = newId; }
+        public void SetDate(DateTime newDate) { dateInit = newDate; }
+        public void SetKm(int newKm) { km = newKm; }
+        public void SetTank(int newTank) { tank = newTank; }
+
+        public string GetId() { return busId; }
+        public DateTime GetDate() { return dateInit; }
+        public int GetKm() { return km; }
+        public int GetTank() { return tank; }
+
+        public static void addBus(DateTime Date, string Id)
+        {
+            Bus bus = new Bus();
+            bus.SetDate(Date);
+            bus.SetBusId(Id);
+
+        }
+        public static Bus ChooseBus(string Id, LinkedList<Bus> busList)
+        {
+            //new Random(DateTime.Now.Millisecond)
+            Bus gg = new Bus();
+            gg = busList.First.Value;
+            //while (gg.GetId != Id)
+            //{
+            //    str
+            //}
+
+            Console.WriteLine("ERROR id doesn't exist");
+            return null;
+        }
     }
 } 
