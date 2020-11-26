e37d83953dfcc38ccb2c86f087cbcd6bb04a1a75
samuel verse <verse@g.jct.ac.il>
2020-11-26 22:58:38 +0200

    done

--- dotNet5781_03a_6859_2304/MainWindow.xaml.cs
+++ dotNet5781_03a_6859_2304/MainWindow.xaml.cs
@@ -26,9 +26,12 @@ namespace dotNet5781_03a_6859_2304
         public MainWindow()
         {
             InitializeComponent();
+            Uri iconUri = new Uri("youtube.ico", UriKind.RelativeOrAbsolute);
+            this.Icon = BitmapFrame.Create(iconUri);
+
             busLigneCollection = new BusLineCollection();
             cbBusLines.ItemsSource = busLigneCollection;
-            cbBusLines.DisplayMemberPath = "BusLineNum";
+            cbBusLines.DisplayMemberPath = "Id";
             cbBusLines.SelectedIndex = 0;
             showBusLines((cbBusLines.SelectedValue as BusLine).Id);
         }
@@ -42,12 +45,16 @@ namespace dotNet5781_03a_6859_2304
         private void cbBusLines_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             showBusLines((cbBusLines.SelectedValue as BusLine).Id);
-
         }
-
         private void lbBusLineStations_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            // ListBox.DataContextProperty = cbBusLines[ComboBox.SelectedIndexProperty].index;
+
+        }
+
+        private void tbArea_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
+
 }
