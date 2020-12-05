505133fc192640c0e3b597ba1cf9447ffd371b77
samuel verse <verse@g.jct.ac.il>
2020-12-05 23:51:41 +0200

    add windows

--- dotNet5781_03b_6859_2304/addWin.xaml.cs
+++ dotNet5781_03b_6859_2304/addWin.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,33 +8,28 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace dotNet5781_03b_6859_2304
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logique d'interaction pour addWin.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class addWin : Window
     {
-        public MainWindow()
+        public addWin()
         {
             InitializeComponent();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            addWin win = new addWin();
-            win.Show();
+
         }
 
-        private void buses_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void dateInputNewBus_TextChanged(object sender, TextChangedEventArgs e)
         {
-            bus bus1 = new bus();
-            ListViewItem list = new ListViewItem();
-                
+
         }
-       
     }
 }
