9417a9cd68ce34f858c92cbf8d163b2e582cb1e6
alexandre lejdstrom <yehouda.lejdstrom@gmail.com>
2020-12-28 22:38:29 +0200

    User + Staion class

---
+++ BE/Station.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    /// <summary>
+    /// base element for: Line, Line station, Trip ...
+    /// </summary>
+    public class Station
+    {
+        #region properties
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public double Longitude { get; set; } // like Y-value
+        public double Lattitude { get; set; } // like X-value
+        #endregion
+    }
+}
