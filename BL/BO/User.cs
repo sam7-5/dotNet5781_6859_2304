9274f8085bb880952230062e97bde3025df4758c
samuel verse <verse@g.jct.ac.il>
2020-12-29 22:26:11 +0200

    comment into user class

--- BL/BO/User.cs
+++ BL/BO/User.cs
@@ -11,6 +11,7 @@ namespace BL.BO
     /// </summary>
     public class User
     {
+        /* menahel has no access to users info*/
         #region properties
         public string UserName { get; set; }
         public string Password { get; set;}
--- DAL/DO/User.cs
+++ DAL/DO/User.cs
@@ -11,6 +11,7 @@ namespace DAL.DO
     /// </summary>
     public class User
     {
+        /* menahel has no access to users info*/
         #region properties
         public string UserName { get; set; }
         public string Password { get; set;}
