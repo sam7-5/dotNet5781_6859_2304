940d14581888a9cd299730adb303efb4037e3f4f
alexandre lejdstrom <yehouda.lejdstrom@gmail.com>
2020-12-30 22:05:12 +0200

    create a data source class and other improvement

--- Bl/myBL.cs
+++ Bl/myBL.cs
@@ -3,20 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL;
 
 namespace BL
 {
-    public class myBL: IBL
+    public class myBL
     {
        
-        public DAL.myDAL myDataAcess = new DAL.myDAL();
-        // can't acces to static method Getstation of myDal
-
-       /* public List<DAL.DO.Station> stations()
-        {
-            var sts = myDataAcess.PrintSomething();
-        }
-       */
         
+   
     }
 }
