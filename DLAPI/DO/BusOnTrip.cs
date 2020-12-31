de713db387ab35f34c42e8f7604818aea8d98435
samuel verse <verse@g.jct.ac.il>
2020-12-31 23:21:40 +0200

    dlapi

---
+++ DLAPI/DO/BusOnTrip.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DAL.DO
+{
+    public class BusOnTrip
+    {
+        #region fields
+        private int id;
+        private int licenseNum;
+        private int lineId;
+        private TimeSpan plannedTakeOff;
+        private TimeSpan actualTakeOff;
+        private int prevStation;
+        private TimeSpan prevStationAT;
+        private TimeSpan nextStationAt;
+        #endregion
+
+        #region properties
+        public int Id { get => id; set { id = value; } }
+        public int LicenseNum { get => licenseNum; set { licenseNum = value; } }
+        public int LineId { get => lineId; set { lineId = value; } }
+        public TimeSpan PlannedTakeOff { get => plannedTakeOff; set { plannedTakeOff = value; } }
+        public TimeSpan ActualTakeOff { get => actualTakeOff; set { actualTakeOff = value; } }
+        public int PrevStation { get => prevStation; set { prevStation = value; } }
+        public TimeSpan PrevStationAT { get => prevStationAT; set { prevStationAT = value; } }
+        public TimeSpan NextStationAt { get => nextStationAt; set { nextStationAt = value; } }
+        #endregion
+    }
+}
---
+++ DLAPI/DO/Station.cs
@@ -0,0 +1,17 @@
+﻿namespace DAL.DO
+{
+    /// <summary>
+    /// base element for: Line, Line station, Trip ...
+    /// </summary>
+    public class Station
+    {
+        #region properties
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public double Longitude { get; set; } // Nort South
+        public double Lattitude { get; set; } // Est West
+        public string Address { get; set; }
+        #endregion
+
+    }
+}
