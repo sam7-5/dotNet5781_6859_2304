bf9339d3c91f339c5a69739b7f45300d3a6bd885
samuel verse <verse@g.jct.ac.il>
2021-01-14 19:42:21 +0200

    ibl

--- BL/IBL.cs
+++ BL/IBL.cs
@@ -17,8 +17,8 @@ namespace BL
         IEnumerable<int> GetAllLinesPassThrough(BO.Station station);
         IEnumerable<BO.StationCustom> GetAllPrevCusStations(BO.Station station);
         IEnumerable<BO.Station> GetAllStationsOfArea(BO.Enums.Area area);
-        void AddStation(BO.StationCustom);
-        void UpdateStation(BO.StationCustom);
+        void AddStation(BO.StationCustom station);
+        void UpdateStation(BO.StationCustom station);
         //--------------------------//
         #endregion
 
@@ -31,7 +31,6 @@ namespace BL
         void UpdateLine(BO.Line line, Action<BO.Line> update);
         void DeleteLine(int lineId);
         //-----------------------------------------------------------------//
-        IEnumerable<BO.Line> GetAllLines();
         IEnumerable<BO.StationCustom> GetAllCusStationOfLine(BO.Line line);
         IEnumerable<BO.StationCustom> GetAllCusStationOfLine(int line);
         void DeleteLine(BO.Line line);
