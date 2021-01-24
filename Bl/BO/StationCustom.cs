165bc2a9619c58905cf86ac34f9edcea0cce4428
samuel verse <verse@g.jct.ac.il>
2021-01-24 15:41:40 +0200

    comment

--- Bl/BO/StationCustom.cs
+++ Bl/BO/StationCustom.cs
@@ -4,18 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+///  global station that has all possible requested info from ui
+/// </summary> 
 namespace BO
 {
     public class StationCustom
-    {       
+    {
         public int LineStationIndex { get; set; }
         public int Code { get; set; }
         public string Name { get; set; }
         public double Distance { get; set; }
         public TimeSpan Time { get; set; }
-        public double Longitude { get; set; } 
-        public double Lattitude { get; set; } 
-
+        public double Longitude { get; set; }
+        public double Lattitude { get; set; }
         public string Adress { get; set; }
     }
 }
